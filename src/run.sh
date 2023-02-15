714500d7f0cc8a0899f19a4d1efeb36ada143e00
vlaghe <vlaghe@protonmail.com>
2023-02-16 00:33:31 +0200

    create tmux session + cleaner verbosity

--- src/run.sh
+++ src/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function panic() {
-        echo "[!] $1" && exit 1
+  echo -e "[!] $1" && exit 1
 }
 
 # Check services
@@ -23,15 +23,21 @@ is_conn="$(nmcli conn show --active | grep wifi)"
 wifiIntf="$(echo $is_conn | awk '{print $4}')"
 localIP="$(ip addr show | grep 'inet ' | grep -v '127.0.0.1' | grep -i "$wifiIntf" | awk '{print $2}' | cut -f1 -d'/')"
 
-[ -z "$is_conn" ] && panic "Not connected to wifi: check your wpa_supplicant.conf OR if your interface is up with ip a"
+[ -z "$is_conn" ] && panic "Not connected to wifi: \n - is your mobile hotspot runnning? \n - check your wpa_supplicant.conf \n - is your interface up? exec ip a"
 echo "[+] Connected to $(echo $is_conn | awk '{print $1}') on $wifiIntf with IP: $localIP"
 
 
 # Wait until gps data syncs: available lat & lon
+echo "[*] Waiting for gps lat & lon..."
+counter=0
 while true; do
   has_location="$(gpspipe -w -n 10 | grep -m 1 lon)"
   [[ -n "$has_location" ]] && echo "Got lat & lon. Starting wardriving :D" && break
   sleep 10
+  counter=$((counter+10))
+
+  # if 20 mins passed & no location data, notify user
+  [[ $counter -gt 1200 ]] && echo -e "[?] 20 min have passed & not location data detected...\n\t maybe check cgps/gpsmon manually?"
 done
 
 # Start the wardriving session
@@ -44,5 +50,18 @@ done
 # | kismet |
 # |--------|
 
-# Start Kismet
+# create new tmux session
+tmux new-session -d -s wardriving
 
+# create first window
+tmux rename-window "HTOP"
+tmux send-keys "htop" C-m
+
+# create second window
+tmux new-window -t wardriving:1 -n "GPS/Kismet"
+tmux split-window -v
+tmux send-keys "gpsmon" C-m
+tmux send-keys "cd ~/data && kismet -t $1" C-m
+
+# attach to session
+tmux attach-session -t wardriving
